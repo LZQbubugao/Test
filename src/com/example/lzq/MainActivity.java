98779cad54c09303b9fed5f1f39e7225bd3c5361
LZQbubugao <1120314768@qq.com>
2016-12-05 09:50:00 +0800

    Update MainActivity.java

--- src/com/example/lzq/MainActivity.java
+++ src/com/example/lzq/MainActivity.java
@@ -9,8 +9,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        hahahhahaiashdio;
+        setContentView(R.layout.activity_main);111
     }
 
 
